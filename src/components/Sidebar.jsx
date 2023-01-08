b3b010d71b0428259f8a357a47a89954772ba7a4
ArshFarwaha <arshdeep.farwaha@gmail.com>
2023-01-07 21:32:00 -0500

    sidebar improvements

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,11 +2,36 @@ import React from "react";
 import "./Sidebar.css";
 import { Button } from "@mui/material";
 import CreateIcon from "@mui/icons-material/Create";
+import InboxIcon from "@mui/icons-material/Inbox";
+import SidebarOption from "./SidebarOption";
+import StarIcon from "@mui/icons-material/Star";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import SendIcon from "@mui/icons-material/Send";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <Button startIcon={<CreateIcon fontSize="large" className="sidebar__compose" />}>Compose</Button>
+      <Button
+        startIcon={<CreateIcon fontSize="large" />}
+        className="sidebar__compose"
+      >
+        Compose
+      </Button>
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={54} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={54} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
+      <SidebarOption Icon={SendIcon} title="Sent" number={54} />
+      <SidebarOption Icon={InsertDriveFileIcon} title="Drafts" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
     </div>
   );
 }
