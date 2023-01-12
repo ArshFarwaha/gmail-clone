e17b3387121b01ae7d69ecaed8a7dc5faa0a4f90
ArshFarwaha <arshdeep.farwaha@gmail.com>
2023-01-11 20:35:49 -0500

    firestore

--- src/components/SendMail.jsx
+++ src/components/SendMail.jsx
@@ -3,16 +3,31 @@ import "./SendMail.css";
 import { Button } from "@mui/material";
 import { Close } from "@mui/icons-material";
 import { useForm } from "react-hook-form";
-import { ErrorMessage } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { closeSendMessage } from "../features/mailSlice";
+import { db } from "./firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 function SendMail() {
-  const { register, handleSubmit, watch, formState: errors } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
   const dispatch = useDispatch();
 
   const onSubmit = (formData) => {
     console.log(formData);
+    const email = {
+      to: formData.to,
+      subject: formData.subject,
+      message: formData.message,
+      timestamp: serverTimestamp(),
+    };
+    addDoc(collection(db, "emails"), email);
+
+    dispatch(closeSendMessage());
   };
 
   return (
@@ -26,30 +41,30 @@ function SendMail() {
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          type="text"
+          type="email"
           placeholder="To"
-          name="to"
-          {...register("to", { required: "To field is required" })}
+          {...register("to", { required: true })}
         />
-        <ErrorMessage errors={errors} name="to" />
-
+        {errors.to && (
+          <p className="sendMail__error">'To' field is required!</p>
+        )}
         <input
           type="text"
           placeholder="Subject"
           {...register("subject", { required: true })}
         />
-        {/* {errors.subject && (
-          <p className="sendMail__error">Subject field is required!</p>
-        )} */}
+        {errors.subject && (
+          <p className="sendMail__error">'Subject' field is required!</p>
+        )}
         <input
           type="text"
           className="sendMail__message"
           placeholder="Message"
           {...register("message", { required: true })}
         />
-        {/* {errors.message && (
-          <p className="sendMail__error">Messsage field is required!</p>
-        )} */}
+        {errors.message && (
+          <p className="sendMail__error">'Messsage' field is required!</p>
+        )}
         <div className="sendMail__options">
           <Button className="sendMail__send" variant="contained" type="submit">
             Send
---
+++ src/components/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB68hLdwRXip6BDpzIwmIAnKnKn8oVHyhA",
+  authDomain: "clone-c3f48.firebaseapp.com",
+  projectId: "clone-c3f48",
+  storageBucket: "clone-c3f48.appspot.com",
+  messagingSenderId: "767423314602",
+  appId: "1:767423314602:web:50b59d7d8d3fc71f172ed3",
+  measurementId: "G-6LS6Z98VTX",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { app, db, auth, provider };
